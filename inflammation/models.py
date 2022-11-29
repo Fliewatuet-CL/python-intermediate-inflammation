a864b75b4f68640ac549c4a416e7f78a523312a2
Fliewatuet-CL <c.lichnog@gmail.com>
2022-11-29 14:49:55 +0100

    Docstring improvements

--- inflammation/models.py
+++ inflammation/models.py
@@ -5,6 +5,13 @@ The Model layer is responsible for the 'business logic' part of the software.
 Patients' data is held in an inflammation table (2D array) where each row contains 
 inflammation data for a single patient taken over a number of days 
 and each column represents a single day across all patients.
+
+Functions:
+
+    load_csv - loads a numpy array from a CSV
+    daily_mean - calculates the daily mean values of the inflammation data
+    daily_max - calculates the daily maximum values of the inflammation data
+    daily_min - calculates the daily minimum values of the inflammation data
 """
 
 import numpy as np
@@ -19,16 +26,34 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    
+    :param data: A 2D data array with inflammation data (each row contains
+    measurements for a single patient across all days).
+
+    :returns: An array of mean values of all measurements for each day.
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    
+    :param data: A 2D data array with inflammation data (each row contains
+    measurements for a single patient across all days).
+    
+    :returns: An array of maximum values of all measurements for each day.
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    
+    :param data: A 2D data array with inflammation data (each row contains
+    measurements for a single patient across all days).
+    
+    :returns: An array of minimum values of all measurements for each day.
+    """
     return np.min(data, axis=0)
 
