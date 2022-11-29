6aa08d498b4cc19ce0cdf90be6d929b762c8a4ff
Fliewatuet-CL <c.lichnog@gmail.com>
2022-11-29 15:05:24 +0100

    Style fixes to up the Pylint score.

--- inflammation/models.py
+++ inflammation/models.py
@@ -2,8 +2,8 @@
 
 The Model layer is responsible for the 'business logic' part of the software.
 
-Patients' data is held in an inflammation table (2D array) where each row contains 
-inflammation data for a single patient taken over a number of days 
+Patients' data is held in an inflammation table (2D array) where each row contains
+inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 
 Functions:
@@ -27,10 +27,8 @@ def load_csv(filename):
 
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array.
-    
     :param data: A 2D data array with inflammation data (each row contains
     measurements for a single patient across all days).
-
     :returns: An array of mean values of all measurements for each day.
     """
     return np.mean(data, axis=0)
@@ -38,10 +36,8 @@ def daily_mean(data):
 
 def daily_max(data):
     """Calculate the daily max of a 2D inflammation data array.
-    
     :param data: A 2D data array with inflammation data (each row contains
     measurements for a single patient across all days).
-    
     :returns: An array of maximum values of all measurements for each day.
     """
     return np.max(data, axis=0)
@@ -49,11 +45,9 @@ def daily_max(data):
 
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array.
-    
     :param data: A 2D data array with inflammation data (each row contains
     measurements for a single patient across all days).
-    
     :returns: An array of minimum values of all measurements for each day.
     """
     return np.min(data, axis=0)
-
+    
